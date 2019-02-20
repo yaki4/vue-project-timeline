3d9cd0e6191f4e52a327b8f14a254e7a718c9843
gabriel <gabriel.corbel@gmail.com>
2019-02-20 18:30:43 -0500

    remove data

--- src/App.vue
+++ src/App.vue
@@ -77,66 +77,66 @@
         type: String
       },
 
-      // projects: {
-      //   required: true,
-      //   type: Array
-      // },
-      //
-      // order: {
-      //   required: false,
-      //   default: 'asc',
-      //   type: String
-      // }
+      projects: {
+        required: true,
+        type: Array
+      },
+      
+      order: {
+        required: false,
+        default: 'asc',
+        type: String
+      }
     },
 
     data () {
       return {
         selectedProject: 0,
-        scrollLocked: false,
-        projects: [
-          {
-            title: 'Client one',
-            subtitle: 'Developer',
-            description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                         consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                         cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                         proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-            image: `logo.png`,
-            startYear: 2014,
-            endYear: 2017,
-            color: '#4a63e0'
-          },
-          {
-            title: 'Client two',
-            subtitle: 'Developer',
-            description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                         consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                         cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                         proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-            image: `logo.png`,
-            startYear: 2014,
-            endYear: 2015,
-            color: '#4a63e0'
-          },
-          {
-            title: 'Client three',
-            subtitle: 'Developer',
-            description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                         consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                         cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                         proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-            image: `logo.png`,
-            startYear: 2016,
-            endYear: 2016,
-            color: '#4a63e0'
-          }
-        ]
+        scrollLocked: false
+        // projects: [
+        //   {
+        //     title: 'Client one',
+        //     subtitle: 'Developer',
+        //     description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        //                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        //                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        //                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        //                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        //                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+        //     image: `logo.png`,
+        //     startYear: 2014,
+        //     endYear: 2017,
+        //     color: '#4a63e0'
+        //   },
+        //   {
+        //     title: 'Client two',
+        //     subtitle: 'Developer',
+        //     description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        //                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        //                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        //                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        //                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        //                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+        //     image: `logo.png`,
+        //     startYear: 2014,
+        //     endYear: 2015,
+        //     color: '#4a63e0'
+        //   },
+        //   {
+        //     title: 'Client three',
+        //     subtitle: 'Developer',
+        //     description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        //                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        //                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        //                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        //                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        //                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+        //     image: `logo.png`,
+        //     startYear: 2016,
+        //     endYear: 2016,
+        //     color: '#4a63e0'
+        //   }
+        // ]
       }
     },
 
